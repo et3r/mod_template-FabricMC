aadd55e44448feb8982fb115c1c0409269e0f5d4
et3r <eterx33@gmail.com>
2022-09-01 18:23:10 -0500

    Initial commit

---
+++ src/main/java/net/et3r/firstmod/FirstMod.java
@@ -0,0 +1,16 @@
+package net.et3r.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FirstMod implements ModInitializer {
+
+	public static final String MOD_ID = "firstmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/et3r/firstmod/FirstModClient.java
@@ -0,0 +1,12 @@
+package net.et3r.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstModClient implements ClientModInitializer {
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
